c75b134aace09fc37c4773c265217270fff6917c
Moorehe2 <91582133+Moorehe2@users.noreply.github.com>
2021-12-19 15:17:44 -0500

    Update streamAssign.py

--- streamAssign.py
+++ streamAssign.py
@@ -9,14 +9,15 @@ df = pd.read_csv("https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_50
 df1 = pd.read_csv("https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/inpatient_2015.csv")
 df2 = pd.read_csv("https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/outpatient_2015.csv")
 
-st.dataframe(df)
-st.dataframe(df1)
-st.dataframe(df2)
+st.write(df)
+st.write(df1)
+st.write(df2)
+
+st.markdown("Display Ambulatory Payment Classification")
 
 
+average_total_payments = df2["average_total_paymeents"].value_counts()
 
-st.subheader ('Outpatient Ambulatory Payment Classification(APC) in Stony Brook University Hospital')
-dataframe_pivot = df2.pivot_table(index=['state','city'],values = ['average_total_payments'],aggfunc ='mean')
+st.bar_chart(df2)
 
-st.text("Comparison of Diagnosis Related Groups (DRGs) at Stony Brook Hospital and Montefiore Medical Center")
-st.text(VISUALIZATION)
+
