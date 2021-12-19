7a549eb94c5295347b646e20c424ee8b60e5aff9
Moorehe2 <91582133+Moorehe2@users.noreply.github.com>
2021-12-19 15:47:21 -0500

    Update streamAssign.py

--- streamAssign.py
+++ streamAssign.py
@@ -3,7 +3,7 @@ import streamlit as st
 import numpy as np
 
 st.title("Center for Medicare and Medicaid Services (CMS) HOSPITAL DATASETS IN THE UNITED STATES")
-st.markdown("The datasets are data from CMS for diagnsis, patient experience, discharges, readmission, imaging, procedures, total charges and payments. I will load the datasets and show some visualization)
+st.markdown("The datasets are data from CMS for diagnosis, patient experience, discharges, readmission, imaging, procedures, total charges and payments. I will load the datasets and show visualization.")
 
 df = pd.read_csv("https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/hospital_info.csv")
 df1 = pd.read_csv("https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/inpatient_2015.csv")
