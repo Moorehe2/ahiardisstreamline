aa9b4ef00d69690653a0cd4860273844da3aaf1c
Moorehe2 <91582133+Moorehe2@users.noreply.github.com>
2021-12-19 13:04:19 -0500

    Update streamAssign.py

--- streamAssign.py
+++ streamAssign.py
@@ -2,8 +2,8 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
-st.title(CMS HOSPITAL DATASETS IN THE UNITED STATES)
-st.text (The datasets are data from hospital in the United States from CMS for diagnsis, total charges and payments.  First I will load the datasets then make a comparison between SUNY/Stony Brook University Hospitals with Bronx Lebanon Hospital, St. Barnabas Hospital and Montefiore Medical Center)
+st.title("CMS HOSPITAL DATASETS IN THE UNITED STATES")
+st.text ("The datasets are data from hospital in the United States from CMS for diagnsis, total charges and payments.  First I will load the datasets then make a comparison between SUNY/Stony Brook University Hospitals with Bronx Lebanon Hospital, Montefiore Medical Center and St. Barnabas")
 
 df = pd.read_csv(https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/hospital_info.csv)
 df1 = pd.read_csv(https://raw.githubusercontent.com/hantswilliams/AHI_DataSci_507/main/Deployment_Streamlit/inpatient_2015.csv)
