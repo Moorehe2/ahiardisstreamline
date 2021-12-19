020f08aa08f727840406a42ce0b2ea1b06738534
Moorehe2 <91582133+Moorehe2@users.noreply.github.com>
2021-12-19 14:34:40 -0500

    Update streamAssign.py

--- streamAssign.py
+++ streamAssign.py
@@ -16,7 +16,7 @@ st.dataframe(df2)
 
 
 st.subheader ('Outpatient Ambulatory Payment Classification(APC) in Stony Brook University Hospital')
-dataframe_pivot = df2.pivot_table(index=['state','city'],values = 'average_total_payments'],aggfunc ='mean')
+dataframe_pivot = df2.pivot_table(index=['state','city'],values = ['average_total_payments'],aggfunc ='mean')
 
 st.text("Comparison of Diagnosis Related Groups (DRGs) at Stony Brook Hospital and Montefiore Medical Center")
 st.text(VISUALIZATION)
